1ce73512c67a834c4b19357b4ec1253c785bdd21
tutajm <mtutaj@mcw.edu>
2025-11-24 13:38:31 -0600

    updated dependencies

--- src/main/dist/run.sh
+++ src/main/dist/run.sh
@@ -7,7 +7,7 @@ APPDIR=/home/rgddata/pipelines/$APPNAME
 SERVER=`hostname -s | tr '[a-z]' '[A-Z]'`
 EMAIL_LIST=mtutaj@mcw.edu
 if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST=mtutaj@mcw.edu,jthota@mcw.edu,jdepons@mcw.edu,jrsmith@mcw.edu
+  EMAIL_LIST="mtutaj@mcw.edu jthota@mcw.edu jdepons@mcw.edu jrsmith@mcw.edu"
 fi
 
 cd $APPDIR
@@ -15,4 +15,4 @@ java -Dspring.config=$APPDIR/../properties/default_db2.xml \
     -Dlog4j.configurationFile=file://$APPDIR/properties/log4j2.xml \
     -jar lib/$APPNAME.jar "$@" 2>&1 | tee run.log
 
-mailx -s "[$SERVER] IMEX Interaction Pipeline OK" $EMAIL_LIST < run.log
+mailx -s "[$SERVER] imex-interactions-pipeline OK" $EMAIL_LIST < run.log
